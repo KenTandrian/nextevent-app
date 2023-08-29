e717bb84a40bc6de4701da8654f96852dfd81df3
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2023-08-29 03:42:30 +0000

    fix(ts): add missing types @ db-utils

--- src/helpers/db-utils.ts
+++ src/helpers/db-utils.ts
@@ -1,18 +1,27 @@
-import { MongoClient } from "mongodb";
+import { Document, Filter, MongoClient, OptionalId, Sort } from "mongodb";
 
 export const connectDatabase = async () => {
     const client = await MongoClient.connect(process.env.MONGODB_EVENTS_URI);
     return client;
 }
 
-export const insertDocument = async (client, collection, document) => {
+export const insertDocument = async (
+    client: MongoClient, 
+    collection: string, 
+    document: OptionalId<Document>,
+) => {
     const db = client.db();
     const result = await db.collection(collection).insertOne(document);
 
     return result;
 }
 
-export const getAllDocuments = async (client, collection, sort, filter = {}) => {
+export const getAllDocuments = async (
+    client: MongoClient, 
+    collection: string, 
+    sort: Sort, 
+    filter: Filter<Document> = {},
+) => {
     const db = client.db();
     const doc = await db
         .collection(collection)
